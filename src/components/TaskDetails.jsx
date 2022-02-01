fb781b3ee06fe15aefc04e5b4f73f46b54fec74a
Ruanxbrx <ruan10d5@hotmail.com>
2022-02-01 15:46:59 -0300

    Projeto finalizado

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useParams, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+import Button from './Button';
+import './TaskDetails.css';
+
+
+const TaskDetails  = () => {
+    const params = useParams();
+    const history = useHistory();
+
+    const handleBackButtonClick = ()=>{
+        history.goBack();
+    }
+    return (  
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id voluptatem ratione reiciendis quaerat aliquam ex.</p>
+            </div>
+        </>
+    );
+
+}
+export default TaskDetails;
---
+++ src/main/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function   Main(){
+    return(
+        <h1>Página inicial</h1>
+
+    );
+};
+ 
+export default Main;
